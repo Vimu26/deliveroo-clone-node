d6bb8e7b9c139c4604ace6ecd252b9a4e27ee4ae
akalanka <akalanka@echonlabs.com>
2023-09-11 10:11:37 +0530

    feat: user delete by id done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-
 const userController = require("../controllers/user.controller");
 
-router.route("/user/getAllUsers").get(userController.getAllUsersController);
+router.route("/user/get-all-users").get(userController.getAllUsersController);
 router.route("/user/create-user").post(userController.createUserController);
-// router.route('./user/deleteUser').delete(userController.deleteUser);
-router.route("/user/update-user/:id").patch(userController.updateUserController);
-
+router
+  .route("/user/update-user/:id")
+  .patch(userController.updateUserController);
+router.route("/user/delete-user/:id").delete(userController.deleteUserController);
 
 module.exports = router;
