3ffd6a55eac02f7575c95944de915781cfcad17c
akalanka <akalanka@echonlabs.com>
2023-09-13 19:39:11 +0530

    feat: :art: dish service added and bugs fixed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(userRoutes);
-app.use(restaurantRoutes)
+app.use(restaurantRoutes);
+app.use(dishRoutes);
 
 mongoose.connect("mongodb://127.0.0.1:27017/deliveroo-clone-api",{
     useNewUrlParser: true,
