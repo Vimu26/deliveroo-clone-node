525dcec9228f858a3fb91b631d7ee18f12f201d2
akalanka <akalanka@echonlabs.com>
2023-09-13 20:26:11 +0530

    fix: :art: dish get all bugs fixed

--- src/controllers/dish.controller.js
+++ src/controllers/dish.controller.js
@@ -1,24 +1,21 @@
-const dishService = require('../services/dish.database.service');
+const dishService = require("../services/dish.database.service");
 
-
-const getAllDishesController = () => async (req,res) => {
-    console.log(res)
-try{
- const dishDetails = await dishService.getAllDishesDBservice();
- if (dishDetails) {
-    res.json({ status: true, message: "No Dishes Found", data: dishDetails });
-  } else {
-    res.json({ status: false, message: " Dishes Not Found" });
-  }
-}
-catch(err){
+const getAllDishesController = async (req, res) => {
+  try {
+    const dishDetails = await dishService.getAllDishesDBservice();
+    if (dishDetails) {
+      res.json({ status: true, message: "No Dishes Found", data: dishDetails });
+    } else {
+      res.json({ status: false, message: " Dishes Not Found" });
+    }
+  } catch (err) {
     console
-    .error(err)
-    .status(500)
-    .json({ status: false, message: "Can't Find Dishes" });
-}
-}
+      .error(err)
+      .status(500)
+      .json({ status: false, message: "Can't Find Dishes" });
+  }
+};
 
 module.exports = {
-    getAllDishesController,
-}
+  getAllDishesController,
+};
--- src/services/dish.database.service.js
+++ src/services/dish.database.service.js
@@ -1,16 +1,19 @@
-const dishDetailsModel = require("../models/dish.model")
+const dishDetailsModel = require("../models/dish.model");
 
 const getAllDishesDBservice = () => {
- return dishDetailsModel.find().then((dish) =>{
-    if(dish.length === 0){
-        console.log("No Dishes Found")
-    }
-    return dish;
- }).catch((err) => {
-    throw err;
-  });
-}
+  return dishDetailsModel
+    .find()
+    .then((dish) => {
+      if (dish.length === 0) {
+        console.log("No Dishes Found");
+      }
+      return dish;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 module.exports = {
-    getAllDishesDBservice,
-  };
+  getAllDishesDBservice,
+};
